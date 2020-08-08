5ef81e38ee90f7b4a3a1b108d567e08619d80d80
ian <ianopolous@protonmail.com>
2020-08-08 16:21:59 +0100

    Fix error message

--- src/main/java/io/ipfs/multiaddr/MultiAddress.java
+++ src/main/java/io/ipfs/multiaddr/MultiAddress.java
@@ -49,7 +49,7 @@ public class MultiAddress
         String[] parts = toString().substring(1).split("/");
         if (parts[2].startsWith("tcp") || parts[2].startsWith("udp"))
             return Integer.parseInt(parts[3]);
-        throw new IllegalStateException("This multiaddress doesn't have a tcp port: "+toString());
+        throw new IllegalStateException("This multiaddress doesn't have a port: "+toString());
     }
 
     private static byte[] decodeFromString(String addr) {
