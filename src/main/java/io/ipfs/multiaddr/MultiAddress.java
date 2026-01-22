f067dde9ad7d32e2c6cd065c22fb3659e9ef60ec
Tamas Cservenak <tamas@cservenak.net>
2026-01-22 21:07:57 +0100

    Tidy up project
    Apply same set of changes as to https://github.com/ipfs-shipyard/java-ipfs-http-client
    
    Changes:
    * GH workflow updates (Java 21, use mvnw)
    * GH dependabot enabled for Maven as well
    * apply same .mvn/maven.config (but no checksum as there are no deps)
    * use wrapper + enable sha256 distro checksum
    * reformat same way as ipfs-http-client
    * update to Junit 6.0.2 (drop hamcrest; is unused)
    * no Java source code changes (sans reformat)

--- src/main/java/io/ipfs/multiaddr/MultiAddress.java
+++ src/main/java/io/ipfs/multiaddr/MultiAddress.java
@@ -1,155 +1,149 @@
 package io.ipfs.multiaddr;
 
-import io.ipfs.multihash.*;
+import io.ipfs.multihash.Multihash;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.util.stream.*;
+public class MultiAddress {
+  private final byte[] raw;
 
-public class MultiAddress
-{
-    private final byte[] raw;
+  public MultiAddress(Multihash hash) {
+    this("/ipfs/" + hash.toBase58());
+  }
 
-    public MultiAddress(Multihash hash) {
-        this("/ipfs/" + hash.toBase58());
-    }
+  public MultiAddress(String address) {
+    this(decodeFromString(address));
+  }
 
-    public MultiAddress(String address) {
-        this(decodeFromString(address));
-    }
+  public MultiAddress(byte[] raw) {
+    encodeToString(raw); // check validity
+    this.raw = raw;
+  }
 
-    public MultiAddress(byte[] raw) {
-        encodeToString(raw); // check validity
-        this.raw = raw;
-    }
+  public byte[] getBytes() {
+    return Arrays.copyOfRange(raw, 0, raw.length);
+  }
 
-    public byte[] getBytes() {
-        return Arrays.copyOfRange(raw, 0, raw.length);
-    }
+  public boolean isRelayed() {
+    return has(Protocol.get("p2p-circuit"));
+  }
 
-    public boolean isRelayed() {
-        return has(Protocol.get("p2p-circuit"));
-    }
-    
-    public boolean has(Protocol p) {
-        String[] parts = toString().substring(1).split("/");
-        return Arrays.asList(parts).contains(p.name());
-    }
+  public boolean has(Protocol p) {
+    String[] parts = toString().substring(1).split("/");
+    return Arrays.asList(parts).contains(p.name());
+  }
 
-    public boolean isPublic(boolean testReachable) {
-        String[] parts = toString().substring(1).split("/");
-        try {
-            for (int i = 0; i < parts.length; i++) {
-                if (parts[i].equals(Protocol.Type.IP6ZONE.name))
-                    return true;
-                if (parts[i].equals(Protocol.Type.IP4.name) || parts[i].equals(Protocol.Type.IP6.name)) {
-                    InetAddress ip = InetAddress.getByName(parts[i + 1]);
-                    if (ip.isLoopbackAddress() || ip.isSiteLocalAddress() || ip.isLinkLocalAddress() || ip.isAnyLocalAddress())
-                        return false;
-                    return !testReachable || ip.isReachable(1000);
-                }
-            }
-        } catch (UnknownHostException e) {}
-        catch (IOException e) {}
-        return false;
-    }
-    
-    public boolean isTCPIP() {
-        String[] parts = toString().substring(1).split("/");
-        if (parts.length != 4)
-            return false;
-        if (!parts[0].startsWith("ip"))
-            return false;
-        if (!parts[2].equals("tcp"))
-            return false;
-        return true;
-    }
-
-    public String getHost() {
-        String[] parts = toString().substring(1).split("/");
-        if (parts[0].startsWith("ip") || parts[0].startsWith("dns"))
-            return parts[1];
-        throw new IllegalStateException("This multiaddress doesn't have a host: "+toString());
-    }
-
-    public int getPort() {
-        String[] parts = toString().substring(1).split("/");
-        if (parts[2].startsWith("tcp") || parts[2].startsWith("udp"))
-            return Integer.parseInt(parts[3]);
-        throw new IllegalStateException("This multiaddress doesn't have a port: "+toString());
-    }
-
-    private static byte[] decodeFromString(String addr) {
-        while (addr.endsWith("/"))
-            addr = addr.substring(0, addr.length()-1);
-        String[] parts = addr.split("/");
-        if (parts[0].length() != 0)
-            throw new IllegalStateException("MultiAddress must start with a /");
-
-        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        try {
-            for (int i = 1; i < parts.length;) {
-                String part = parts[i++];
-                Protocol p = Protocol.get(part);
-                p.appendCode(bout);
-                if (p.size() == 0)
-                    continue;
-
-                String component = p.isTerminal() ?
-                        Stream.of(Arrays.copyOfRange(parts, i, parts.length)).reduce("", (a, b) -> a + "/" + b) :
-                        parts[i++];
-                if (component.length() == 0)
-                    throw new IllegalStateException("Protocol requires address, but none provided!");
-
-                bout.write(p.addressToBytes(component));
-                if (p.isTerminal())
-                    break;
-            }
-            return bout.toByteArray();
-        } catch (IOException e) {
-            throw new IllegalStateException("Error decoding multiaddress: "+addr);
+  public boolean isPublic(boolean testReachable) {
+    String[] parts = toString().substring(1).split("/");
+    try {
+      for (int i = 0; i < parts.length; i++) {
+        if (parts[i].equals(Protocol.Type.IP6ZONE.name)) return true;
+        if (parts[i].equals(Protocol.Type.IP4.name) || parts[i].equals(Protocol.Type.IP6.name)) {
+          InetAddress ip = InetAddress.getByName(parts[i + 1]);
+          if (ip.isLoopbackAddress()
+              || ip.isSiteLocalAddress()
+              || ip.isLinkLocalAddress()
+              || ip.isAnyLocalAddress()) return false;
+          return !testReachable || ip.isReachable(1000);
         }
+      }
+    } catch (UnknownHostException e) {
+    } catch (IOException e) {
+    }
+    return false;
+  }
+
+  public boolean isTCPIP() {
+    String[] parts = toString().substring(1).split("/");
+    if (parts.length != 4) return false;
+    if (!parts[0].startsWith("ip")) return false;
+    if (!parts[2].equals("tcp")) return false;
+    return true;
+  }
+
+  public String getHost() {
+    String[] parts = toString().substring(1).split("/");
+    if (parts[0].startsWith("ip") || parts[0].startsWith("dns")) return parts[1];
+    throw new IllegalStateException("This multiaddress doesn't have a host: " + toString());
+  }
+
+  public int getPort() {
+    String[] parts = toString().substring(1).split("/");
+    if (parts[2].startsWith("tcp") || parts[2].startsWith("udp")) return Integer.parseInt(parts[3]);
+    throw new IllegalStateException("This multiaddress doesn't have a port: " + toString());
+  }
+
+  private static byte[] decodeFromString(String addr) {
+    while (addr.endsWith("/")) addr = addr.substring(0, addr.length() - 1);
+    String[] parts = addr.split("/");
+    if (parts[0].length() != 0) throw new IllegalStateException("MultiAddress must start with a /");
+
+    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+    try {
+      for (int i = 1; i < parts.length; ) {
+        String part = parts[i++];
+        Protocol p = Protocol.get(part);
+        p.appendCode(bout);
+        if (p.size() == 0) continue;
+
+        String component =
+            p.isTerminal()
+                ? Stream.of(Arrays.copyOfRange(parts, i, parts.length))
+                    .reduce("", (a, b) -> a + "/" + b)
+                : parts[i++];
+        if (component.length() == 0)
+          throw new IllegalStateException("Protocol requires address, but none provided!");
+
+        bout.write(p.addressToBytes(component));
+        if (p.isTerminal()) break;
+      }
+      return bout.toByteArray();
+    } catch (IOException e) {
+      throw new IllegalStateException("Error decoding multiaddress: " + addr);
+    }
+  }
+
+  private static String encodeToString(byte[] raw) {
+    StringBuilder b = new StringBuilder();
+    InputStream in = new ByteArrayInputStream(raw);
+    try {
+      while (true) {
+        int code = (int) Protocol.readVarint(in);
+        Protocol p = Protocol.get(code);
+        b.append("/" + p.name());
+        if (p.size() == 0) continue;
+
+        String addr = p.readAddress(in);
+        if (addr.length() > 0) b.append("/" + addr);
+      }
+    } catch (EOFException eof) {
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
 
-    private static String encodeToString(byte[] raw) {
-        StringBuilder b = new StringBuilder();
-        InputStream in = new ByteArrayInputStream(raw);
-        try {
-            while (true) {
-                int code = (int)Protocol.readVarint(in);
-                Protocol p = Protocol.get(code);
-                b.append("/" + p.name());
-                if (p.size() == 0)
-                    continue;
+    return b.toString();
+  }
 
-                String addr = p.readAddress(in);
-                if (addr.length() > 0)
-                    b.append("/" +addr);
-            }
-        }
-        catch (EOFException eof) {}
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+  @Override
+  public String toString() {
+    return encodeToString(raw);
+  }
 
-        return b.toString();
-    }
+  @Override
+  public boolean equals(Object other) {
+    if (!(other instanceof MultiAddress)) return false;
+    return Arrays.equals(raw, ((MultiAddress) other).raw);
+  }
 
-    @Override
-    public String toString() {
-        return encodeToString(raw);
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (!(other instanceof MultiAddress))
-            return false;
-        return Arrays.equals(raw, ((MultiAddress) other).raw);
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(raw);
-    }
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(raw);
+  }
 }
--- src/test/java/io/ipfs/api/MultiAddressTest.java
+++ src/test/java/io/ipfs/api/MultiAddressTest.java
@@ -1,23 +1,24 @@
 package io.ipfs.api;
 
-import io.ipfs.multiaddr.*;
-import io.ipfs.multihash.*;
-import org.junit.jupiter.api.Test;
-
-import java.io.*;
-import java.util.*;
-import java.util.function.*;
-import java.util.stream.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.ipfs.multiaddr.MultiAddress;
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+
 public class MultiAddressTest {
 
-    @Test
-    void fails() {
-        List<String> parsed = Stream.of(
+  @Test
+  void fails() {
+    List<String> parsed =
+        Stream.of(
                 "/ip4",
                 "/ip4/::1",
                 "/ip4/fdpsofodsajfdoisa",
@@ -64,58 +65,61 @@ public class MultiAddressTest {
                 "/ip4/127.0.0.1/tcp",
                 "/ip4/127.0.0.1/ipfs",
                 "/ip4/127.0.0.1/ipfs/tcp",
-                "/unix"
-        ).flatMap(s -> {
-            try {
-                new MultiAddress(s);
-                return Stream.of(s);
-            } catch (Exception e) {
-                return Stream.empty();
-            }
-        }).collect(Collectors.toList());
-        if (parsed.size() > 0)
-            throw new IllegalStateException("Parsed invalid MultiAddresses: "+parsed);
-    }
+                "/unix")
+            .flatMap(
+                s -> {
+                  try {
+                    new MultiAddress(s);
+                    return Stream.of(s);
+                  } catch (Exception e) {
+                    return Stream.empty();
+                  }
+                })
+            .collect(Collectors.toList());
+    if (parsed.size() > 0)
+      throw new IllegalStateException("Parsed invalid MultiAddresses: " + parsed);
+  }
 
-    @Test
-    void publicIps() {
-        List<MultiAddress> pub = Stream.of("/ip4/8.8.8.8")
-                .map(MultiAddress::new)
-                .collect(Collectors.toList());
-        for (MultiAddress addr : pub) {
-            assertTrue(addr.isPublic(false), addr.toString() + " is public");
-        }
-        List<MultiAddress> priv = Stream.of(
-//                "/ip4/100.64.0.0",
-                        "/ip4/172.16.0.0",
-                        "/ip4/10.0.0.0",
-                        "/ip4/169.254.0.0",
-                        "/ip4/192.168.0.0",
-                        "/ip4/127.0.0.1",
-                        "/ip4/0.0.0.0",
-//                        "/ip4/192.0.0.0",
-//                        "/ip4/192.0.2.0",
-//                        "/ip4/192.88.99.0",
-//                        "/ip4/198.18.0.0",
-//                        "/ip4/198.51.100.0",
-//                        "/ip4/203.0.113.0",
-//                        "/ip4/224.0.0.0",
-//                        "/ip4/240.0.0.0",
-//                        "/ip4/255.255.255.255",
-                        "/ip6/::1",
-//                        "/ip6/fc00::",
-//                        "/ip6/ff00::",
-                        "/ip6/fe80::"
-                ).map(MultiAddress::new)
-                .collect(Collectors.toList());
-        for (MultiAddress addr : priv) {
-            assertFalse(addr.isPublic(false), addr.toString() + " is private");
-        }
+  @Test
+  void publicIps() {
+    List<MultiAddress> pub =
+        Stream.of("/ip4/8.8.8.8").map(MultiAddress::new).collect(Collectors.toList());
+    for (MultiAddress addr : pub) {
+      assertTrue(addr.isPublic(false), addr.toString() + " is public");
     }
+    List<MultiAddress> priv =
+        Stream.of(
+                //                "/ip4/100.64.0.0",
+                "/ip4/172.16.0.0",
+                "/ip4/10.0.0.0",
+                "/ip4/169.254.0.0",
+                "/ip4/192.168.0.0",
+                "/ip4/127.0.0.1",
+                "/ip4/0.0.0.0",
+                //                        "/ip4/192.0.0.0",
+                //                        "/ip4/192.0.2.0",
+                //                        "/ip4/192.88.99.0",
+                //                        "/ip4/198.18.0.0",
+                //                        "/ip4/198.51.100.0",
+                //                        "/ip4/203.0.113.0",
+                //                        "/ip4/224.0.0.0",
+                //                        "/ip4/240.0.0.0",
+                //                        "/ip4/255.255.255.255",
+                "/ip6/::1",
+                //                        "/ip6/fc00::",
+                //                        "/ip6/ff00::",
+                "/ip6/fe80::")
+            .map(MultiAddress::new)
+            .collect(Collectors.toList());
+    for (MultiAddress addr : priv) {
+      assertFalse(addr.isPublic(false), addr.toString() + " is private");
+    }
+  }
 
-    @Test
-    void succeeds() {
-        List<String> failed = Stream.of(
+  @Test
+  void succeeds() {
+    List<String> failed =
+        Stream.of(
                 "/ip4/1.2.3.4",
                 "/ip4/0.0.0.0",
                 "/ip4/192.0.2.0/ipcidr/24",
@@ -200,100 +204,100 @@ public class MultiAddressTest {
                 "/ip4/127.0.0.1/tcp/127/tls/ws",
                 "/ip4/127.0.0.1/tcp/127/noise",
                 "/ip4/127.0.0.1/tcp/127/wss",
-                "/ip4/127.0.0.1/tcp/127/wss"
-        ).flatMap(s -> {
-            try {
-                new MultiAddress(s);
-                return Stream.empty();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return Stream.of(s);
-            }
-        }).collect(Collectors.toList());
-        if (failed.size() > 0)
-            throw new IllegalStateException("Failed to construct MultiAddresses: "+failed);
-    }
+                "/ip4/127.0.0.1/tcp/127/wss")
+            .flatMap(
+                s -> {
+                  try {
+                    new MultiAddress(s);
+                    return Stream.empty();
+                  } catch (Exception e) {
+                    e.printStackTrace();
+                    return Stream.of(s);
+                  }
+                })
+            .collect(Collectors.toList());
+    if (failed.size() > 0)
+      throw new IllegalStateException("Failed to construct MultiAddresses: " + failed);
+  }
 
-    @Test
-    void equalsTests() {
-        MultiAddress m1 = new MultiAddress("/ip4/127.0.0.1/udp/1234");
-        MultiAddress m2 = new MultiAddress("/ip4/127.0.0.1/tcp/1234");
-        MultiAddress m3 = new MultiAddress("/ip4/127.0.0.1/tcp/1234");
-        MultiAddress m4 = new MultiAddress("/ip4/127.0.0.1/tcp/1234/");
+  @Test
+  void equalsTests() {
+    MultiAddress m1 = new MultiAddress("/ip4/127.0.0.1/udp/1234");
+    MultiAddress m2 = new MultiAddress("/ip4/127.0.0.1/tcp/1234");
+    MultiAddress m3 = new MultiAddress("/ip4/127.0.0.1/tcp/1234");
+    MultiAddress m4 = new MultiAddress("/ip4/127.0.0.1/tcp/1234/");
 
-        if (m1.equals(m2))
-            throw new IllegalStateException("Should be unequal!");
+    if (m1.equals(m2)) throw new IllegalStateException("Should be unequal!");
 
-        if (m2.equals(m1))
-            throw new IllegalStateException("Should be unequal!");
+    if (m2.equals(m1)) throw new IllegalStateException("Should be unequal!");
 
-        if (!m2.equals(m3))
-            throw new IllegalStateException("Should be equal!");
+    if (!m2.equals(m3)) throw new IllegalStateException("Should be equal!");
 
-        if (!m3.equals(m2))
-            throw new IllegalStateException("Should be equal!");
+    if (!m3.equals(m2)) throw new IllegalStateException("Should be equal!");
 
-        if (!m1.equals(m1))
-            throw new IllegalStateException("Should be equal!");
+    if (!m1.equals(m1)) throw new IllegalStateException("Should be equal!");
 
-        if (!m2.equals(m4))
-            throw new IllegalStateException("Should be equal!");
+    if (!m2.equals(m4)) throw new IllegalStateException("Should be equal!");
 
-        if (!m4.equals(m3))
-            throw new IllegalStateException("Should be equal!");
-    }
+    if (!m4.equals(m3)) throw new IllegalStateException("Should be equal!");
+  }
 
-    @Test
-    void conversion() {
-        BiConsumer<String, String> test = (s, h) -> {
-            if (!s.equals(new MultiAddress(fromHex(h)).toString()))
-                throw new IllegalStateException(s + " != " + new MultiAddress(fromHex(h)));
-            if (! Arrays.equals(new MultiAddress(s).getBytes(), fromHex(h)))
-                throw new IllegalStateException("bytes for " + s + " != " + fromHex(h));
+  @Test
+  void conversion() {
+    BiConsumer<String, String> test =
+        (s, h) -> {
+          if (!s.equals(new MultiAddress(fromHex(h)).toString()))
+            throw new IllegalStateException(s + " != " + new MultiAddress(fromHex(h)));
+          if (!Arrays.equals(new MultiAddress(s).getBytes(), fromHex(h)))
+            throw new IllegalStateException("bytes for " + s + " != " + fromHex(h));
         };
 
-        test.accept("/ip4/159.89.141.29/udp/5491/quic", "049f598d1d91021573cc03");
-        test.accept("/ip4/127.0.0.1/udp/1234", "047f000001910204d2");
-        test.accept("/ip4/127.0.0.1/tcp/4321", "047f0000010610e1");
-        test.accept("/ip4/127.0.0.1/udp/1234/ip4/127.0.0.1/tcp/4321", "047f000001910204d2047f0000010610e1");
-        test.accept("/onion/aaimaq4ygg2iegci:80", "bc030010c0439831b48218480050");
-        test.accept("/onion3/vww6ybal4bd7szmgncyruucpgfkqahzddi37ktceo3ah7ngmcopnpyyd:1234", "bd03adadec040be047f9658668b11a504f3155001f231a37f54c4476c07fb4cc139ed7e30304d2");
-        test.accept("/garlic64/jT~IyXaoauTni6N4517EG8mrFUKpy0IlgZh-EY9csMAk82Odatmzr~YTZy8Hv7u~wvkg75EFNOyqb~nAPg-" +
-                "khyp2TS~ObUz8WlqYAM2VlEzJ7wJB91P-cUlKF18zSzVoJFmsrcQHZCirSbWoOknS6iNmsGRh5KVZsBEfp1Dg3gwTipTRIx7Vl5V" +
-                "y~1OSKQVjYiGZS9q8RL0MF~7xFiKxZDLbPxk0AK9TzGGqm~wMTI2HS0Gm4Ycy8LYPVmLvGonIBYndg2bJC7WLuF6tVjVquiokSVD" +
-                "KFwq70BCUU5AU-EvdOD5KEOAM7mPfw-gJUG4tm1TtvcobrObqoRnmhXPTBTN5H7qDD12AvlwFGnfAlBXjuP4xOUAISL5SRLiulrs" +
-                "MSiT4GcugSI80mF6sdB0zWRgL1yyvoVWeTBn1TqjO27alr95DGTluuSqrNAxgpQzCKEWAyzrQkBfo2avGAmmz2NaHaAvYbOg0QSJ" +
-                "z1PLjv2jdPW~ofiQmrGWM1cd~1cCqAAAA",
-                "be0383038d3fc8c976a86ae4e78ba378e75ec41bc9ab1542a9cb422581987e118f5cb0c024f3639d6ad9b3aff613672f0" +
-                        "7bfbbbfc2f920ef910534ecaa6ff9c03e0fa4872a764d2fce6d4cfc5a5a9800cd95944cc9ef0241f753fe71494a1" +
-                        "75f334b35682459acadc4076428ab49b5a83a49d2ea2366b06461e4a559b0111fa750e0de0c138a94d1231ed5979" +
-                        "572ff53922905636221994bdabc44bd0c17fef11622b16432db3f193400af53cc61aa9bfc0c4c8d874b41a6e1873" +
-                        "2f0b60f5662ef1a89c80589dd8366c90bb58bb85ead56356aba2a244950ca170abbd01094539014f84bdd383e4a1" +
-                        "0e00cee63dfc3e809506e2d9b54edbdca1bace6eaa119e68573d30533791fba830f5d80be5c051a77c09415e3b8f" +
-                        "e3139400848be5244b8ae96bb0c4a24f819cba0488f34985eac741d3359180bd72cafa1559e4c19f54ea8cedbb6a" +
-                        "5afde4319396eb92aab340c60a50cc2284580cb3ad09017e8d9abc60269b3d8d687680bd86ce834412273d4f2e3b" +
-                        "f68dd3d6fe87e2426ac658cd5c77fd5c0aa000000");
-        test.accept("/garlic32/566niximlxdzpanmn4qouucvua3k7neniwss47li5r6ugoertzuq",
-                "bf0320efbcd45d0c5dc79781ac6f20ea5055a036afb48d45a52e7d68ec7d4338919e69");
-    }
+    test.accept("/ip4/159.89.141.29/udp/5491/quic", "049f598d1d91021573cc03");
+    test.accept("/ip4/127.0.0.1/udp/1234", "047f000001910204d2");
+    test.accept("/ip4/127.0.0.1/tcp/4321", "047f0000010610e1");
+    test.accept(
+        "/ip4/127.0.0.1/udp/1234/ip4/127.0.0.1/tcp/4321", "047f000001910204d2047f0000010610e1");
+    test.accept("/onion/aaimaq4ygg2iegci:80", "bc030010c0439831b48218480050");
+    test.accept(
+        "/onion3/vww6ybal4bd7szmgncyruucpgfkqahzddi37ktceo3ah7ngmcopnpyyd:1234",
+        "bd03adadec040be047f9658668b11a504f3155001f231a37f54c4476c07fb4cc139ed7e30304d2");
+    test.accept(
+        "/garlic64/jT~IyXaoauTni6N4517EG8mrFUKpy0IlgZh-EY9csMAk82Odatmzr~YTZy8Hv7u~wvkg75EFNOyqb~nAPg-"
+            + "khyp2TS~ObUz8WlqYAM2VlEzJ7wJB91P-cUlKF18zSzVoJFmsrcQHZCirSbWoOknS6iNmsGRh5KVZsBEfp1Dg3gwTipTRIx7Vl5V"
+            + "y~1OSKQVjYiGZS9q8RL0MF~7xFiKxZDLbPxk0AK9TzGGqm~wMTI2HS0Gm4Ycy8LYPVmLvGonIBYndg2bJC7WLuF6tVjVquiokSVD"
+            + "KFwq70BCUU5AU-EvdOD5KEOAM7mPfw-gJUG4tm1TtvcobrObqoRnmhXPTBTN5H7qDD12AvlwFGnfAlBXjuP4xOUAISL5SRLiulrs"
+            + "MSiT4GcugSI80mF6sdB0zWRgL1yyvoVWeTBn1TqjO27alr95DGTluuSqrNAxgpQzCKEWAyzrQkBfo2avGAmmz2NaHaAvYbOg0QSJ"
+            + "z1PLjv2jdPW~ofiQmrGWM1cd~1cCqAAAA",
+        "be0383038d3fc8c976a86ae4e78ba378e75ec41bc9ab1542a9cb422581987e118f5cb0c024f3639d6ad9b3aff613672f0"
+            + "7bfbbbfc2f920ef910534ecaa6ff9c03e0fa4872a764d2fce6d4cfc5a5a9800cd95944cc9ef0241f753fe71494a1"
+            + "75f334b35682459acadc4076428ab49b5a83a49d2ea2366b06461e4a559b0111fa750e0de0c138a94d1231ed5979"
+            + "572ff53922905636221994bdabc44bd0c17fef11622b16432db3f193400af53cc61aa9bfc0c4c8d874b41a6e1873"
+            + "2f0b60f5662ef1a89c80589dd8366c90bb58bb85ead56356aba2a244950ca170abbd01094539014f84bdd383e4a1"
+            + "0e00cee63dfc3e809506e2d9b54edbdca1bace6eaa119e68573d30533791fba830f5d80be5c051a77c09415e3b8f"
+            + "e3139400848be5244b8ae96bb0c4a24f819cba0488f34985eac741d3359180bd72cafa1559e4c19f54ea8cedbb6a"
+            + "5afde4319396eb92aab340c60a50cc2284580cb3ad09017e8d9abc60269b3d8d687680bd86ce834412273d4f2e3b"
+            + "f68dd3d6fe87e2426ac658cd5c77fd5c0aa000000");
+    test.accept(
+        "/garlic32/566niximlxdzpanmn4qouucvua3k7neniwss47li5r6ugoertzuq",
+        "bf0320efbcd45d0c5dc79781ac6f20ea5055a036afb48d45a52e7d68ec7d4338919e69");
+  }
 
-    @Test
-    void dns() {
-       String dns = "mydomain.com";
-       int port = 5001;
-       String address = "/dns6/"+dns+"/tcp/"+port+"/https";
-       MultiAddress multiAddress = new MultiAddress(address);
-       
-       assertEquals(dns, multiAddress.getHost(), "host should be equal to " + dns);
-       assertEquals(port, multiAddress.getPort(), "port should be equal to " + port);
-    }
+  @Test
+  void dns() {
+    String dns = "mydomain.com";
+    int port = 5001;
+    String address = "/dns6/" + dns + "/tcp/" + port + "/https";
+    MultiAddress multiAddress = new MultiAddress(address);
 
-    public static byte[] fromHex(String hex) {
-        if (hex.length() % 2 != 0)
-            throw new IllegalStateException("Uneven number of hex digits!");
-        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        for (int i=0; i < hex.length()-1; i+= 2)
-            bout.write(Integer.valueOf(hex.substring(i, i+2), 16));
-        return bout.toByteArray();
-    }
+    assertEquals(dns, multiAddress.getHost(), "host should be equal to " + dns);
+    assertEquals(port, multiAddress.getPort(), "port should be equal to " + port);
+  }
+
+  public static byte[] fromHex(String hex) {
+    if (hex.length() % 2 != 0) throw new IllegalStateException("Uneven number of hex digits!");
+    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+    for (int i = 0; i < hex.length() - 1; i += 2)
+      bout.write(Integer.valueOf(hex.substring(i, i + 2), 16));
+    return bout.toByteArray();
+  }
 }
